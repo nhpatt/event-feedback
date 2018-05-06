233c553acb57e0e553e8baf62e09c06e81a7758e
Javier Gamarra <nhpatt@gmail.com>
2018-05-07 00:54:32 +0200

    redirect in https

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const basicAuth = require('express-basic-auth');
+const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
 
 const indexController = require('./controllers/index');
 const talksController = require('./controllers/talks');
@@ -12,7 +13,7 @@ module.exports = (app) => {
     app.use('/', express.static(path.join(__dirname, '/favicons')));
     app.use('/static', express.static(path.join(__dirname, '/static')));
     app.use('/admin-new', express.static(path.join(__dirname, '/admin/ui')));
-
+    app.use(redirectToHTTPS([/localhost:(\d{4})/], [/\/insecure/], 301));
     app.get('/', indexController.getIndex);
     app.get('/talk/:slug', talksController.getTalkById);
     app.post('/talk/:slug', talksController.postFeedback);
